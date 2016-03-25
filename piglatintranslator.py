4de375b91eb7e7e7e9c4105b56360bbcdd916b13
arg2211 <arg2211@columbia.edu>
2016-03-25 10:43:59 -0400

    write translator

--- piglatintranslator.py
+++ piglatintranslator.py
@@ -3,16 +3,17 @@ code inspired by Codeacademy Python lesson called "PygLatin"
 available here: https://www.codecademy.com/learn/python
 '''
 
-print 'Pig Latin Translator' #prints this string
+print 'This is a Pig Latin Translator' #prints this string
 
-word = raw_input("Type any word:") #word = a string that person inputs after seeing the command 'type any word'
+ay = 'ay'
+
+word = raw_input('Type any word here:') #word = a string that person inputs after seeing the command 'type any word'
 if len(word) > 0 and word.isalpha(): #only prints if the string entered has at least 1 character and all characters are letters only
 
     word = word.lower() #makes word lowercase
-    letter1 = word[0] #get first letter of the word
-    letter2 = word[1] #get second letter of the word
-    letter3 = word[2] #get third letter of the word
+    letter1 = word[0] #get first letter of the word *positions in a string start at zero!*
+    pigword = word[1:len(word)] + letter1 + ay #concatenate strings with +
+    print 'the translation of ' + word + ' is ' + pigword
 
-    print word and "nice work!"
 else: #prints line below if word does not meet 'if' parameters
-    print "that's not a word! please use letters only"
+    print 'that\'s not a word! please use letters only'
