c345c0c7bab7664b1dddda124d12507ca73a7d1f
arg2211 <arg2211@columbia.edu>
2016-06-08 17:19:30 -0400

    add raw_input

--- mathfunction.py
+++ mathfunction.py
@@ -1,18 +1,18 @@
 '''
 This is a function called 'mathfun' where there are two parameters, n1 and n2.
-We call 'mathfun' with the arguments 1 and 30 in the last line.
+We can call 'mathfun' with the arguments n1 and n2 in the last line.
+n1 and n2 are input by the user.
 '''
 
-def mathfun(n1, n2):  # define the function, called 'mathfun' and put names of parameters inside parentheses
-    base = n1 + n2  # define variables named 'base', 'exponent', and 'result'
+# define the function, called 'mathfun' and put names of parameters inside parentheses
+def mathfun(n1, n2):
+    # define variables named 'base', 'exponent', and 'result'
+    base = n1 + n2
     exponent = n1 - n2
     result = base**exponent
+    # and tell the function what to print
     print "First you entered %d, then you entered %d. I added them together to get your base, %d.  I subtracted the second entry from the first to get your exponent, %d.  %d to the power of %d is %d!" % (n1, n2, base, exponent, base, exponent, result)
 
-
-#    if len(n1 > 0) and len(n2 > 0) and n1.isnumerical() and n2.isnumerical():
-#        print "First you entered %d, then you entered %d. I added them together to get your base, %d.  I subtracted the second entry from the first to get your exponent, %d.  %d to the power of %d is %d!" % (n1, n2, base, exponent, base, exponent, result)
-#    else:
-#        print "Oops, make sure you entered real numbers!"
-
-mathfun(1, 30)
+n1 = int(raw_input("Pick a number:"))
+n2 = int(raw_input("Pick another number:"))
+mathfun(n1, n2)
